78e56de734820274f17c1943504e7c82d6482ada
Ken Bartlett <kabartlett@me.com>
2016-02-09 17:06:51 +0000

    added edit and creation of users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,33 @@
 class UsersController < ApplicationController
   def new
+    @title = "Signup"
     @user = User.new
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the beta blog #{@user.username}"
+      flash[:success] = "Welcome to the beta blog, #{@user.username}!"
       redirect_to articles_path
     else
       @title = "Signup"
       render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit #{@user.username}"
+  end
+    
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully!"
+      redirect_to articles_path
+    else
+      @title = "Edit #{@user.username}"
+      render 'edit'
     end
   end
   
